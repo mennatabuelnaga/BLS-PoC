f3e520da2db64f18d10ca3b792e0799e81b8f8c3
mnaga <mennat.abuelnaga@gmail.com>
2022-11-06 23:47:11 +0200

    feat: initialize altbn128

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,19 +1,70 @@
 
-use bls_signatures::{Signature, PublicKey, Serialize};
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::near_bindgen;
+use near_sys::alt_bn128_pairing_check;
+use zeropool_bn::{G2, Group};
+
+#[repr(C)]
+struct PairingElem([u8; 64], [u8; 128]); // (G1, G2) uncompressed
+
 
 #[near_bindgen]
 #[derive(Default, BorshDeserialize, BorshSerialize)]
 pub struct VerifySignature;
-// deployment storage ~ 22 NEAR
 #[near_bindgen]
 impl VerifySignature {
+    // e(H(m)_G1, pk_G2) = e(sig_G1, G2)
+    // e(sig_G1, nG2) e(H(m)_G1, pk_G2) =? 1
+
+    // e(nG1, sig_G2) e(pk_G1, H(m)_G2)
+
+    // (hash_g1, pk_g2)  (sig_g1, -G2)
+   
+
     #[payable]
-    pub fn verify_sig(&mut self, message: Vec<u8>, signature: Vec<u8>, public_key: Vec<u8>) -> bool {
-        let sig = Signature::from_bytes(&signature[..]).expect("couldn't read signature");
-        let pk = PublicKey::from_bytes(&public_key[..]).expect("couldn't read pk");
-        pk.verify(sig, message)
+    pub fn verify_sig(&mut self, message_hash: Vec<u8>, signature: Vec<u8>, public_key: Vec<u8>) -> bool {
+        let hash_g1: [u8; 64] = message_hash.try_into().unwrap();
+        let pk_g2: [u8; 128] = public_key.try_into().unwrap();
+        let sig_g1: [u8; 64] = signature.try_into().unwrap();
+        let n_g2: [u8; 128] = (-G2::one()).try_to_vec().unwrap().try_into().unwrap();
+        println!("*******************************************");
+
+        println!("hash_g1 = : {:?}", hash_g1);
+        println!("*******************************************");
+        println!("pk_g2 = : {:?}", pk_g2);
+        println!("*******************************************");
+
+
+        println!("sig_g1 = : {:?}", sig_g1);
+        println!("*******************************************");
+
+        println!("n_g2 = : {:?}", n_g2);
+        println!("*******************************************");
+
+
+
+        let buf = [(hash_g1, pk_g2), (sig_g1, n_g2)];
+
+        // let g1: [u8; 64] = [80, 12, 4, 181, 61, 254, 153, 52, 127, 228, 174, 24, 144, 95, 235, 26, 197, 188, 219, 91, 4, 47, 98, 98, 202, 199, 94, 67, 211, 223, 197, 21, 65, 221, 184, 75, 69, 202, 13, 56, 6, 233, 217, 146, 159, 141, 116, 208, 81, 224, 146, 124, 150, 114, 218, 196, 192, 233, 253, 31, 130, 152, 144, 29];
+        // let g2: [u8; 128] = [34, 54, 229, 82, 80, 13, 200, 53, 254, 193, 250, 1, 205, 60, 38, 172, 237, 29, 18, 82, 187, 98, 113, 152, 184, 251, 223, 42, 104, 148, 253, 25, 79, 39, 165, 18, 195, 165, 215, 155, 168, 251, 250, 2, 215, 214, 193, 172, 187, 84, 54, 168, 27, 100, 161, 155, 144, 95, 199, 238, 88, 238, 202, 46, 247, 97, 33, 56, 78, 174, 171, 15, 245, 5, 121, 144, 88, 81, 102, 133, 118, 222, 81, 214, 74, 169, 27, 91, 27, 23, 80, 55, 43, 97, 101, 24, 168, 29, 75, 136, 229, 2, 55, 77, 60, 200, 227, 210, 172, 194, 232, 45, 151, 46, 248, 206, 193, 250, 145, 84, 78, 176, 74, 210, 0, 106, 168, 30];
+        
+        // let buf: [PairingElem; 2] = [PairingElem(g1, g2), PairingElem(g1, g2)];
+        
+       
+
+
+        let mut res = 0;
+       
+        unsafe {
+                res = alt_bn128_pairing_check(
+                    core::mem::size_of_val(&buf) as u64,
+                    buf.as_ptr() as *const u64 as u64,
+                );
+            }
+            println!("{:?}", res);
+            res == 1
+
+
         
     }
 }
@@ -21,16 +72,15 @@ impl VerifySignature {
 #[cfg(not(target_arch = "wasm32"))]
 #[cfg(test)]
 mod tests {
+
     use super::*;
+    use bls_signatures_rs::MultiSignature;
+    use bls_signatures_rs::bn256::{Bn256, PublicKey};
     use near_sdk::test_utils::VMContextBuilder;
     use near_sdk::{testing_env, VMContext};
-
-    use bls_signatures::{PrivateKey, Serialize};
-    use rand_chacha::ChaCha8Rng;
-    use rand_chacha::rand_core::SeedableRng;
-
-
-
+    use zeropool_bn::G1;
+    // use near_sys::alt_bn128_pairing_check;
+    
     fn get_context(is_view: bool) -> VMContext {
         VMContextBuilder::new()
             .signer_account_id("bob_near".parse().unwrap())
@@ -44,35 +94,49 @@ mod tests {
         testing_env!(context);
         let mut contract = VerifySignature::default();
 
+        // let rng = &mut rand::thread_rng();
+
+        let sk = hex::decode("a55e93edb1350916bf5beea1b13d8f198ef410033445bcb645b65be5432722f1").unwrap();
 
 
-        let mut rng = ChaCha8Rng::seed_from_u64(12);
+        let pk = Bn256.derive_public_key(&sk).unwrap();
 
-        let sk = PrivateKey::generate(&mut rng);
+        let message: &[u8] = b"sample";
 
-        let pk = sk.public_key();
+        let sig = Bn256.sign(&sk, &message).unwrap();
 
-        let message = "hello".try_to_vec().unwrap();
+        // println!("sk: {:?}", sk);
+        // println!("pk: {:?}", pk);
+        // println!("sig: {:?}", sig);
+
+        println!("sk_bytes len: {:?}", sk.len()); // 32
+
+        println!("pk_bytes len: {:?}", pk.len()); // 65
+
+        println!("sig_bytes len: {:?}", sig.len()); //33
+        let pk_2 = PublicKey::from_compressed(pk.as_ref()).expect("pk error");
+        
+
+        println!("pk2 len: {:?}", pk_2.to_uncompressed().unwrap().len()); // 128
 
         
-        let sig = sk.sign(message.clone());
-        
-        println!("sk: {:?}", sk);
-        println!("pk: {:?}", pk);
-        println!("sig: {:?}", sig);
+        let x = G1::from_compressed(sig.as_ref()).unwrap();
+        let y = x.try_to_vec().unwrap();
 
-        println!("pk_bytes: {:?}", pk.as_bytes());
-        println!("sig_bytes: {:?}", sig.as_bytes());
-        println!("sig_bytes: {:?}", sig.as_bytes());
+        println!("sig2 len: {:?}", y.len()); // 64
 
+        let hash = Bn256.hash_to_try_and_increment2(message).unwrap();
+        let hash_bytes = hash.try_to_vec().unwrap();
+        println!("hash_bytes len: {:?}", hash_bytes.len()); //64
 
-        let sig_bytes = sig.as_bytes();
-
-        let pk_bytes= pk.as_bytes();
-        let verified = contract.verify_sig(message, sig_bytes, pk_bytes);
+        // Check whether the aggregated signature corresponds to the aggregated public key
+        Bn256.verify(&sig, &message, &pk).unwrap();
+        let verified = contract.verify_sig(hash_bytes, y, pk_2.to_uncompressed().unwrap(), );
+        println!("YES?????? {:?}", verified);
 
 
-        println!("verified: {:?}", verified);
+
+
 
 
     }
